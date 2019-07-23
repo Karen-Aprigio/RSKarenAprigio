cb2dc293f677ff35083d3feb6a3fd185826216d2
igorIBE <eduardobatistaigor@gmail.com>
2019-07-23 16:40:12 -0300

    Projeto Java RS1 TP1 criado

---
+++ JAVA RS KAREN/Projeto Java/Aluno/src/aluno/Aluno.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aluno;
+
+/**
+ *
+ * @author aluno
+ */
+public class Aluno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     String Nome;
+     String Matricula;
+     String CPF;
+     String RG;
+     String CR;
+             }
+}
+
+     
+     
+     
+       
+        // TODO code application logic here
+    }
+    
+}
